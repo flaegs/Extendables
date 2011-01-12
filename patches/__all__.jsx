ccfad1ac030d748574403d658d8bb526f5c17e00
Jonas Oesch <flaegs@gmail.com>
2011-01-12 12:56:48 +0100

    Methode getXPath("XPath") für Objekte des Typs "XMLElement" hinzugefügt. Gibt für ein Array mit den Resultaten zurück.

--- patches/__all__.jsx
+++ patches/__all__.jsx
@@ -1,15 +1,1 @@
-﻿#include "object.jsx"
-#include "string.jsx"
-#include "array.jsx"
-#include "object.conversions.jsx"
-#include "error.jsx"
-#include "file.jsx"
-#include "date.jsx"
-#include "math.jsx"
-#include "dom.application.jsx"
-if (!app.is("toolkit")) {
-	#include "dom.suite.jsx"
-}
-if (app.is("indesign")) {
-	#include "dom.indesign.jsx"
-}
+﻿#include "object.jsx"#include "string.jsx"#include "array.jsx"#include "object.conversions.jsx"#include "error.jsx"#include "file.jsx"#include "date.jsx"#include "math.jsx"#include "dom.application.jsx"if (!app.is("toolkit")) {	#include "dom.suite.jsx"}if (app.is("indesign")) {	#include "dom.indesign.jsx"	#include "xml.jsx"}
